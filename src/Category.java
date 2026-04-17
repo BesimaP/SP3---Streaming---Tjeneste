bca4b17ef00499446c2e6488b28bcd5a13bab9f4
BesimaP <bepl1000@stud.ek.dk>
2026-04-17 09:54:01 +0200

    enum done med smileyer

--- src/Category.java
+++ src/Category.java
@@ -1,2 +1,20 @@
 public enum Category {
+    Crime("🔪"),
+    War("⚔️"),
+    Drama("🎭"),
+    Family("👨‍👩‍👧"),
+    Romance("❤️"),
+    Sciencefiction("🚀");
+
+    private final String smiley;
+
+    Category(String smiley) {
+        this.smiley = smiley;
+    }
+
+    public String getSmiley() {
+        return smiley;
+    }
+
+    //eksempel: Category.Romance + " " + Category.Romance.getSmiley()
 }
