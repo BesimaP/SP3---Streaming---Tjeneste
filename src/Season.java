fc8edfc3dc66c6390adae0eb933168742bb3147d
BesimaP <bepl1000@stud.ek.dk>
2026-04-17 10:13:29 +0200

    season klasse opsat

--- src/Season.java
+++ src/Season.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Season {
+    private int seasonNumber;
+    private int year;
+    private ArrayList<Episode> episodes;
+
+
+    public int getSeasonNumber(){
+        return seasonNumber;
+    }
+
+    public ArrayList<Episode> getEpisodes() {
+        return episodes;
+    }
+
+    public void addEpisode(Episode e) {
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
